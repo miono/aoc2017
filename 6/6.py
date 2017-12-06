e67e28c312b4ccc271a9dfafdac241b75efb0bb6
Markus Nilsson <miono@users.noreply.github.com>
2017-12-06 07:20:43 +0100

    Day 6

---
+++ 6/6.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+f = open('input').readlines()[0]
+f = f.split('	')
+cur_bank = map(int, f)
+cycles = 0
+# print f
+
+all_results = []
+all_results.append(cur_bank)
+
+def redistribute(bank_config):
+  global cycles
+  cycles += 1
+  m = max(bank_config)
+  bank_to_distribute = [i for i, j in enumerate(bank_config) if j == m][0]
+  num_steps = bank_config[bank_to_distribute]
+  bank_config[bank_to_distribute] = 0
+  for x in xrange(num_steps):
+    bank_config[(bank_to_distribute + x + 1) % len(bank_config)] += 1
+  if bank_config in all_results:
+    print len(all_results)
+    print len(all_results) - all_results.index(bank_config)
+    exit()
+  return bank_config
+
+while True:
+  all_results.append(redistribute(list(all_results[-1])))
